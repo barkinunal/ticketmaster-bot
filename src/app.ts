9f8ffe82fd65caa3a7b4687e6f873332ddb70ead
Barkın Ünal <barkinunal@Barkns-MacBook-Pro.local>
2022-03-21 20:25:47 +0000

    Home page added.

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 import * as ticketmaster from "./ticketmaster";
 import { Telegraf } from "telegraf";
 import * as logger from "./utils/logUtils";
-
+const { env } = process;
 export const app = express();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
@@ -11,3 +11,12 @@ bot.launch();
 setInterval(() => {
   ticketmaster.getAppEventDetails(bot);
 }, 30 * 1000);
+
+app.get("/", (req, res) => {
+  logger.info('"/" is requested.');
+  res.send("Hello World!");
+});
+
+app.listen(env.PORT, () => {
+  return console.log(`Express is listening at http://localhost:${env.PORT}`);
+});
