3ff82fcdd64eb5a88c593900067158708d406878
Barkın Ünal <barkinunal@gmail.com>
2022-03-21 21:39:24 +0000

    Chat ids updated

--- src/ticketmaster.ts
+++ src/ticketmaster.ts
@@ -12,9 +12,12 @@ export async function getAppEventDetails(bot: Telegraf) {
 
   if (response.status === 200) {
     logger.info("THERE IS A TICKET!!!");
-    await bot.telegram.sendMessage(env.BOT_CHAT_ID, "THERE IS A TICKET");
+    await bot.telegram.sendMessage(env.BOT_FOUND_CHAT_ID, "THERE IS A TICKET");
   } else {
     logger.info("There are no tickets yet :(");
-    bot.telegram.sendMessage(env.BOT_CHAT_ID, "There are no tickets yet :(");
+    await bot.telegram.sendMessage(
+      env.BOT_COULDNT_FOUND_CHAT_ID,
+      "There are no tickets yet :("
+    );
   }
 }
